e429d60cc185b551b39300bf7d086d328fa71bac
elena-code-star <elena.corio@students.iaac.net>
2026-03-01 17:41:35 +0100

    Write and test functions for creating speckle model

---
+++ src/adapters/create_speckle_model.py
@@ -0,0 +1,61 @@
+from config import AUTHORS, FUNCTION, SOURCE_MODEL_ID
+from domain.metrics.all_metrics import calculate_metrics
+from domain.model.elements import Model, ModelElement
+from domain.model.model_filter import filter_model
+
+from specklepy.objects.base import Base
+
+def create_base(name: str, model: Model, properties: dict):
+    """
+    Create a Speckle Base object with the given name, model, and properties and empty elements list.
+    """
+    base = Base()
+    base.name = name
+    base["properties"] = properties
+    base["metrics"] = calculate_metrics(model)
+    base.elements = []
+    return base
+
+def create_element(element: ModelElement, name: str, model: Model, properties: dict):
+    """
+    Create a Speckle element with the geometry from the ModelElement, given name, model, and properties.
+    """
+    element = element.geometry
+    element.name = name
+    element["properties"] = properties
+    element["metrics"] = calculate_metrics(model)
+    return element
+
+def model_to_speckle(model: Model):
+    """
+    Build hierarchical metrics structure as Speckle Base objects for project, clusters, and levels.
+    """
+    speckle_model = create_base(
+        name = "Hyperbuilding_03", 
+        model = model, 
+        properties = {
+        "authors": AUTHORS, 
+        "function": FUNCTION, 
+        "source": SOURCE_MODEL_ID})
+
+    for cluster_id in model.cluster_ids():
+        cluster_model = filter_model(model, lambda e: e.cluster_id == cluster_id)
+        cluster_base = create_base(
+            name=f"Cluster {cluster_id}", 
+            model=cluster_model, 
+            properties={"cluster_id": cluster_id})
+
+        for level in model.levels_for_cluster(cluster_id):
+            level_model = filter_model(cluster_model, lambda e: e.level == level)
+            level_element = next(iter(level_model.volumes), None)  # Volumes represent geometry for the level
+            level_geometry = create_element(
+                element=level_element, 
+                name=f"Level {level}", 
+                model=level_model, 
+                properties={"cluster_id": cluster_id, "level": level}
+            )
+            cluster_base.elements.append(level_geometry)
+
+        speckle_model.elements.append(cluster_base)
+
+    return speckle_model
--- src/config.py
+++ src/config.py
@@ -5,3 +5,6 @@ WORKSPACE_ID = "a1cd06bae2"
 PROJECT_ID = "dcca94731b"
 SOURCE_MODEL_ID = "827526cd48"
 TARGET_MODEL_ID = "a17c364985"
+
+AUTHORS = ["Elena Corio, Symon Kipkemei"]
+FUNCTION = "digital-tissue-automate"
