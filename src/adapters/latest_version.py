a7ffb190c5398f9d642c6a99d437aa67186e97cc
elena-code-star <elena.corio@students.iaac.net>
2026-03-01 14:24:15 +0100

    Write and test function for fetching latest version of a model

---
+++ src/adapters/latest_version.py
@@ -0,0 +1,13 @@
+from config import PROJECT_ID, SOURCE_MODEL_ID
+from specklepy.api.client import SpeckleClient
+
+def get_latest_version(client: SpeckleClient):
+    # Get the latest version
+    versions = client.version.get_versions(SOURCE_MODEL_ID, PROJECT_ID, limit=1)
+    if not versions.items:
+        print("No versions found.")
+        return
+    latest_version = versions.items[0]
+    print(f"✓ Fetching version: {latest_version.id}")
+    
+    return latest_version
